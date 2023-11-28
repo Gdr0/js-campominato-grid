c49fa7f135c1954079cb55a93d868bc6c9a0fdd7
Gabriele Di Rocco <gabriele.dirocco000@gmail.com>
2023-11-28 17:59:56 +0100

    bonus

--- script.js
+++ script.js
@@ -1,24 +1,61 @@
 const gridElement = document.getElementById("grid");
 const buttonPlay = document.getElementById("play");
+const difficile = document.getElementById("difficile");
+const facile = document.getElementById("facile");
 
-buttonPlay.addEventListener(
-  "click",
-  function () {
-    for (let i = 1; i <= 100; i++) {
-      const newElement = createMyElement("div", "square");
-      newElement.addEventListener("click", function () {
-        this.classList.add("clicked");
-        console.log(i);
-      });
-      gridElement.append(newElement);
-      newElement.append(i);
-    }
-  },
-  { once: true }
-);
+// buttonPlay.addEventListener(
+//   "click",
+//   function () {
+//     for (let i = 1; i <= 100; i++) {
+//       const newElement = createMyElement("div", "square");
+//       newElement.addEventListener("click", function () {
+//         this.classList.add("clicked");
+//         console.log(i);
+//       });
+//       gridElement.append(newElement);
+//       newElement.append(i);
+//     }
+//   },
+//   { once: true }
+// );
 
 function createMyElement(tagtype, classname) {
   const currentElement = document.createElement(tagtype);
   currentElement.classList.add(classname);
   return currentElement;
 }
+facile.addEventListener("click", function () {
+  buttonPlay.addEventListener(
+    "click",
+    function () {
+      for (let i = 1; i <= 100; i++) {
+        const newElement = createMyElement("div", "square");
+        newElement.addEventListener("click", function () {
+          this.classList.add("clicked");
+          console.log(i);
+        });
+        gridElement.append(newElement);
+        newElement.append(i);
+      }
+    },
+    { once: true }
+  );
+});
+
+difficile.addEventListener("click", function () {
+  buttonPlay.addEventListener(
+    "click",
+    function () {
+      for (let i = 1; i <= 81; i++) {
+        const newElement = createMyElement("div", "square-diff");
+        newElement.addEventListener("click", function () {
+          this.classList.add("clicked");
+          console.log(i);
+        });
+        gridElement.append(newElement);
+        newElement.append(i);
+      }
+    },
+    { once: true }
+  );
+});
